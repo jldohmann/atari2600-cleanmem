8cfde74fc000e31c7b567d8726d5d26f68feed01
unknown <jldohmann@utexas.edu>
2020-09-01 22:58:27 -0500

    name change and re-assemble

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 c:\cc65\bin\ca65 src\cleanmem.asm
-c:\cc65\bin\ld65 src\cleanmem.o -C atari2600.cfg -o cleanmem.bin
+c:\cc65\bin\ld65 src\cleanmem.o -C atari2600.cfg -o cart.bin
 del src\*.o /q
